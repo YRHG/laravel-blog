017f603a68cb2b7f264045fd10512afe49e47880
YRHG <975816342hhz@gmail.com>
2025-05-20 20:58:31 +0900

    Update _header.blade.php

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,26 +2,26 @@
     <div class="container">
         <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', "AlbertHan's Blog") }}</a>
 
-        {{-- Navbar Toggler Button --}}
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        {{-- Collapsible Navbar Content --}}
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> {{-- ms-auto 将导航项推向右侧 --}}
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                {{-- 所有人都能看到 --}}
+                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+
                 @if (Auth::check())
                     <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             {{ Auth::user()->name }}
                         </a>
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"> {{-- dropdown-menu-end 使下拉菜单右对齐 --}}
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
                             <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
                             <div class="dropdown-divider"></div>
-                            {{-- Logout Form - styled button as dropdown item --}}
                             <form action="{{ route('logout') }}" method="POST">
                                 @csrf
                                 @method('DELETE')
@@ -30,7 +30,6 @@
                         </div>
                     </li>
                 @else
-                    <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                 @endif
             </ul>
